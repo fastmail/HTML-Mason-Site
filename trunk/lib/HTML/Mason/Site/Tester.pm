f0444e2b3d05a526d15a07d860cc5805e1c12afc
hdp <hdp@1bcdbe44-fcfd-0310-b51b-975661d93aa0>
2006-07-26 15:27:55 +0000

    r26241@vex:  hdp | 2006-07-26 11:27:41 -0400  first draft of testing object
    git-svn-id: svn+ssh://svn.pobox.com/svn/HTML-Mason-Site@22104 1bcdbe44-fcfd-0310-b51b-975661d93aa0

---
+++ trunk/lib/HTML/Mason/Site/Tester.pm
@@ -0,0 +1,91 @@
+package HTML::Mason::Site::Tester;
+
+use strict;
+use warnings;
+use HTML::Mason::Site;
+use HTML::Mason::Site::CGIHandler;
+
+use HTTP::Request;
+use HTTP::Request::AsCGI;
+
+=head1 NAME
+
+HTML::Mason::Site::Tester
+
+=head1 SYNOPSIS
+
+  use HTML::Mason::Site::Tester;
+  my $tester = HTML::Mason::Site::Tester->new(...);
+  my $response = $tester->request("/foo");
+
+=head1 DESCRIPTION
+
+HTML::Mason::Site::Tester objects make it easier to test Mason
+applications.  Each tester object is tied to a
+HTML::Mason::Site object, and makes requests to it without
+needing an external webserver.
+
+=head1 METHODS
+
+=head2 new
+
+  my $tester = HTML::Mason::Site::Tester->new(...);
+
+Create a new tester object associated with a particular site.
+
+The first (mandatory) argument is a hashref which is passed
+into HTML::Mason::Site-E<gt>new.
+
+=head2 request
+
+  my $response = $tester->request('/foo?bar=1');
+
+Make a request for the given path.  Returns a HTTP::Response object.
+
+=head2 get
+
+  my $content = $tester->get('/foo?bar=1');
+
+Make a request for the given path, and return the content.
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $site_config = shift;
+  my $site = HTML::Mason::Site->new($site_config);
+  my $handler = HTML::Mason::Site::CGIHandler->new(
+    $site->mason_config,
+  );
+  $handler->site($site);
+  $site->require_modules;
+
+  return bless {
+    site    => $site,
+    handler => $handler,
+  } => $class;
+}
+
+sub _request_from_url {
+  my ($self, $url) = @_;
+  $url = "http://localhost.localdomain$url" if $url =~ m!^/!;
+  return HTTP::Request->new( GET => $url );
+}
+
+sub request {
+  my ($self, $request) = @_;
+  $request = $self->_request_from_url($request)
+    unless eval { $request->isa('HTTP::Request') };
+
+  my $c = HTTP::Request::AsCGI->new( $request )->setup;
+  $self->{handler}->handle_request;
+  $c->restore;
+
+  my $response = $c->response;
+  
+  $response->header('Content-Base' => $request->uri);
+
+  return $response;
+}
+
+1;
