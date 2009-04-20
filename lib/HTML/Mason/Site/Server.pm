cb67f8a9114a14276e21c072cddc0222be094047
Ricardo SIGNES <rjbs@cpan.org>
2009-04-20 13:30:47 -0400

    globally disable alarm stuff

--- lib/HTML/Mason/Site/Server.pm
+++ lib/HTML/Mason/Site/Server.pm
@@ -31,8 +31,8 @@ sub new {
   my ($class, $port, $arg) = @_;
   my $self = $class->SUPER::new($port);
 
-  $self->{__PACKAGE__}{alarm} = $arg->{alarm} || 0;
-  alarm $self->{__PACKAGE__}{alarm};
+  # $self->{__PACKAGE__}{alarm} = $arg->{alarm} || 0;
+  # alarm $self->{__PACKAGE__}{alarm};
 
   return $self;
 }
@@ -149,7 +149,7 @@ sub handle_request {
   my $self = shift;
   my ($cgi) = @_;
 
-  alarm 0;
+  # alarm 0;
 
   $self->content_type(undef);
   # this should probably be done by HTTP::Server::Simple, but... workaround for
@@ -186,7 +186,7 @@ sub handle_request {
     
     if ($static_ok->($content_type)) {
       $self->_handle_static($filename, $content_type);
-      alarm($self->{'HTML::Mason::Site::Server'}{alarm} || 0);
+      # alarm($self->{'HTML::Mason::Site::Server'}{alarm} || 0);
       return;
     }
     last;
@@ -212,7 +212,7 @@ sub handle_request {
 
   # $self->cleanup
   # return values are not meaningful
-  alarm($self->{'HTML::Mason::Site::Server'}{alarm} || 0);
+  # alarm($self->{'HTML::Mason::Site::Server'}{alarm} || 0);
 }
 
 =head2 handle_error
